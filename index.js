a885115411c4691a7e393aab206932331fac5103
B-gooroom <j.sean1316@gmail.com>
2022-02-12 12:40:01 +0900

    feat: playBall animation

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const playBoard = document.getElementById('play-board');
 const playBall = document.getElementById('play-ball');
 let speed = 50;
 const ballSize = 10;
+const direction = {
+  letf: 0,
+  top: ballSize
+}
 
 playBoard.style.width = '300px';
 playBoard.style.height = '300px';
@@ -10,14 +14,20 @@ playBall.style.height = '10px';
 console.log(playBoard.style)
 
 const setting = function() {
-  playBall.style.left = '145px'
-  playBall.style.top = '145px'
+  playBall.style.left = '140px';
+  playBall.style.top = '140px';
   speed = 50;
 };
 
 const render = function() {
-  console.log(speed)
-  playBall.style.top = parseInt(playBall.style.top) + ballSize + 'px';
+  console.log(speed,parseInt(playBall.style.top))
+
+  if (parseInt(playBall.style.top) >= 290) {
+    direction.top = ballSize * -1;
+  } else if (parseInt(playBall.style.top) <= 0) {
+    direction.top = ballSize * 1;
+  }
+  playBall.style.top = parseInt(playBall.style.top) + direction.top + 'px';
   setTimeout(render, speed);
 }
 
