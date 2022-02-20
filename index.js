8520c838afdf9252d9a030c4a27a797b57b54bea
B-gooroom <j.sean1316@gmail.com>
2022-02-20 12:06:12 +0900

    feat: bar 좌/우 이동

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const direction = {
   left: -10,
   top: ballSize
 }
+let barRedDirection = 0;
+let barGreenDirection = 0;
 const barWidth = 90; 
 let go = true;
 
@@ -21,6 +23,24 @@ barGreen.style.width = barWidth + 'px';
 barGreen.style.height = ballSize + 'px';
 console.log(playBoard.style)
 
+const player1 = function(value) {
+  console.log(barRedDirection, value)
+  if (barRedDirection === value) {
+    barRedDirection = 0;
+  } else {
+    barRedDirection = value;
+  }
+}
+
+const player2 = function(value) {
+  console.log(barGreenDirection, value)
+  if (barGreenDirection === value) {
+    barGreenDirection = 0;
+  } else {
+    barGreenDirection = value;
+  }
+}
+
 const setting = function() {
   playBall.style.left = '140px';
   playBall.style.top = '140px';
@@ -33,7 +53,7 @@ const setting = function() {
 
 const render = function() {
   if (!go) return;
-  console.log(speed,parseInt(playBall.style.top))
+  // console.log(speed,parseInt(playBall.style.top))
   
   if (parseInt(playBall.style.top) >= 270) {
     direction.top = ballSize * -1;
@@ -46,6 +66,23 @@ const render = function() {
   } else if (parseInt(playBall.style.left) <= 0) {
     direction.left = ballSize * 1;
   }
+
+  if (barRedDirection !== 0) {
+    if (
+      (parseInt(barRed.style.left) > 0 && barRedDirection === -10) ||
+      (parseInt(barRed.style.left) < 210 && barRedDirection === 10)
+    ) {
+      barRed.style.left = parseInt(barRed.style.left) + barRedDirection + 'px';
+    }
+  }
+  if (barGreenDirection !== 0) {
+    if (
+      (parseInt(barGreen.style.left) > 0 && barGreenDirection === -10) ||
+      (parseInt(barGreen.style.left) < 210 && barGreenDirection === 10)
+    ) {
+      barGreen.style.left = parseInt(barGreen.style.left) + barGreenDirection + 'px';
+    }
+  }
   playBall.style.top = parseInt(playBall.style.top) + direction.top + 'px';
   playBall.style.left = parseInt(playBall.style.left) + direction.left + 'px';
   setTimeout(render, speed);
@@ -53,6 +90,11 @@ const render = function() {
 
 const stop = function () {
   go = false;
+}
+
+const start = function () {
+  go = true;
+  setTimeout(render, speed);
 }
 setting();
 setTimeout(render, speed);
